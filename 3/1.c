ddd18119bd8322657354082ce5a559675f51444f
aelael <dlagh@yandex.com>
2019-01-17 19:42:53 +0300

    3_1 done

---
+++ 3/1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+  // put your code here
+  printf("S.Holmes:\n");
+  printf("51grad 31\'25.48\" N\n");
+  printf("0 grad  9\'29.93\" W");
+  return 0;
+}
---
+++ 3/3_1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+  // put your code here
+  return 0;
+}
